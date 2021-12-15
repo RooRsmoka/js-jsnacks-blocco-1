495bbc2d7b109ebffe9b02daedd039b4faf652d4
RooRsmoka <chri.disa96@gmail.com>
2021-12-15 20:14:24 +0100

    completato snack-4

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -1,2 +1,14 @@
 // SNACK-4: Chiedi un numero di 4 cifre all’utente e calcola
 // la somma di tutte le cifre che compongono il numero.
+
+//
+const userNumber = prompt('inserisci un numero di quattro cifre.');
+//
+let sum = 0;
+//
+for(let i = 0; i < 4 ; i++) {
+    sum += parseInt(userNumber[i]); 
+};
+
+console.log(userNumber);
+console.log(sum);
